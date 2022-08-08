fcb7e051d8689ceacca22ef9e7165817962c3605
Victor Adepoju <victoradepoju30@gmail.com>
2022-08-08 15:53:56 +0100

    refactored custom button

--- lib/custom button/custom_button.dart
+++ lib/custom button/custom_button.dart
@@ -1,4 +1,7 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:custom_3d_button/reuseables/bottom_botton.dart';
+import 'package:custom_3d_button/reuseables/top_button.dart';
+import 'package:custom_3d_button/reuseables/text_box.dart';
 import 'package:flutter/material.dart';
 
 //PositionedTransition
@@ -31,89 +34,9 @@ class CustomButton extends StatelessWidget {
           Positioned(
             top: 10 * pushDepth,
             left: 0,
-            child: const TextBox(buttonText: 'PUSH ME'),
+            child: TextBox(buttonText: 'PUSH ME'),
           ),
         ],
-      ),
-    );
-  }
-}
-
-class TopButton extends StatelessWidget {
-  final Color topColor;
-  const TopButton({
-    Key? key,
-    required this.topColor,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 50,
-      width: 300,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(30),
-        // border: Border.all(color: Colors.grey, width: 2),
-        color: topColor,
-        boxShadow: const [
-          BoxShadow(
-            blurRadius: 10,
-            color: Colors.grey,
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class BottomButton extends StatelessWidget {
-  final Color topColor;
-  const BottomButton({
-    Key? key,
-    required this.topColor,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 50,
-      width: 300,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(30),
-        // border: Border.all(color: Colors.grey, width: 2),
-        color: topColor,
-        boxShadow: const [
-          BoxShadow(
-            blurRadius: 10,
-            color: Colors.grey,
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class TextBox extends StatelessWidget {
-  final String buttonText;
-  const TextBox({
-    Key? key,
-    required this.buttonText,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 50,
-      width: 300,
-      child: Center(
-        child: Text(
-          buttonText,
-          style: const TextStyle(
-            fontSize: 20,
-            color: Colors.white,
-            fontWeight: FontWeight.w900,
-          ),
-        ),
       ),
     );
   }
